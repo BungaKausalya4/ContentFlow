04fb95233a40eb4914381a13a2ead56097dc6faf
evitadewinh <160727513+evitadewinh@users.noreply.github.com>
2024-10-30 16:02:34 +0700

    update analysis dan recommendation

--- mysite/Analisis/views.py
+++ mysite/Analisis/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def LandingPage(request):
     return render(request, 'landingpage.html')
+
+def AnalysisURL(request):
+    return render(request, 'analysis-url.html')
+
+def RecommendationURL(request):
+    return render(request, 'recommendation-url.html')
+
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,10 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Analisis.views import LandingPage
+from Analisis.views import AnalysisURL
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPage),
+    path('analysis/', AnalysisURL, name=""),
+    path('recommendation/', AnalysisURL, name=""),
+
 ]
